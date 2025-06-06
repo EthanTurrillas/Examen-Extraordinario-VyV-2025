07caba91ab45e05ea8dc494dfd2dc0c6420f3ad4
EthanTurrillas <ethanturrillas8@gmail.com>
2025-06-06 16:33:24 +0200

    [refactor] - Se divide el codigo en funciones para las diferentes funcionalidades(prestamo, devolucion)

--- src/GestionBiblioteca.php
+++ src/GestionBiblioteca.php
@@ -7,7 +7,14 @@ class GestionBiblioteca
     public function gestionPrestamos($cadena)
     {
         $partes = explode(' ', $cadena);
+        if ($partes[0] == 'prestar') {
+            return $this->prestamo($partes);
+        }
+        return "Error: Comando no reconocido.";
+    }
 
+    private function prestamo($partes)
+    {
         if (count($partes) <= 1) {
             return "Error: No se ha especificado un libro para prestar.";
         }
